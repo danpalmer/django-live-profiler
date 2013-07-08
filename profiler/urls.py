5aa74411c007bf809982262acb1acc031b5092a6
Stefano Crosta <stefano.crosta@gmail.com>
2013-07-08 12:26:21 +0200

    django.conf.urls.defaults is deprecated
    Safely try importing django.conf.urls first, django.conf.urls.defaults later.

--- profiler/urls.py
+++ profiler/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import patterns, include, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns(
     'profiler.views',
